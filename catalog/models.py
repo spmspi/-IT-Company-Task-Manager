77902c081748c9db05fe938cc6cb9f2d2401e50b
SPMSPI <kislicin.artur@gmail.com>
2026-07-04 12:55:42 +0300

    Add tempates and urls (path index), read views add def index

--- catalog/models.py
+++ catalog/models.py
@@ -5,13 +5,13 @@ from django.db import models
 class TaskType(models.Model):
     name = models.CharField(max_length=255)
 
+
 class Position(models.Model):
     name = models.CharField(max_length=255)
 
 
 class Worker(AbstractUser):
     position = models.ForeignKey(Position, on_delete=models.CASCADE)
-
 
 
 class Task(models.Model):
@@ -22,5 +22,3 @@ class Task(models.Model):
     priority = models.TextChoices("Low", "Medium", "High")
     task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
     assigness = models.ManyToManyField(Worker, related_name="assigned_tasks")
-
-
--- catalog/views.py
+++ catalog/views.py
@@ -1,2 +1,16 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+
+from catalog import models
+
+
+@login_required
+def index(request):
+    num_tasks = models.Task.objects.all().count()
+    num_workers = models.Worker.objects.all().count()
+    context = {
+        "num_tasks": num_tasks,
+        "num_workers": num_workers,
+    }
+    return render(request, "manager/index.html", context=context)
 
