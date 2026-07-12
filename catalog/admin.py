36b9862ea0d3916ae360ce3067d908196b7ca714
SPMSPI <kislicin.artur@gmail.com>
2026-07-12 08:21:43 +0300

    Read admin.py add task admin (filter in deadline) and worker admin fieldsets position

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Worker, Task
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "deadline")
+    list_filter = ("deadline",)
+
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = UserAdmin.list_display + ("position", )
+    fieldsets = UserAdmin.fieldsets + (
+        (("Position info", {"fields": ("position",)}),)
+    )
+
+admin.site.register(Worker, WorkerAdmin)
+
+
